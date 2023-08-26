af8fd72737938761216b4bb8cf77f372025f83a2
mateo <mateovg.1943@gmail.com>
2023-08-25 19:13:43 -0500

    Python projects

---
+++ dayOneProject.py
@@ -0,0 +1,4 @@
+companyName = str(input("Cual es el nombre de tu compañia: "))
+hobbieName = str(input("Cual es tu hobbie favorito: "))
+
+print(f"El nombre de tu nueva cerveza es \n\"{hobbieName} by {companyName}\" taste it now!")
